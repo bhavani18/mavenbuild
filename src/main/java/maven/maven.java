fac53749a5b66c92644e8e49505d7dcab3f187ab
athukuribhavani <bhavani.athukuri85@gmail.com>
2018-03-05 12:26:42 +0530

    maven commit

---
+++ src/main/java/maven/maven.java
@@ -0,0 +1,11 @@
+package maven;
+
+public class maven {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("bbkb bbbbbjjj");
+		System.out.println("dvdsjsjdjsjdk");
+	}
+
+}
